09cca74300e2c39e0ac89f05e50cd22ad5e0aa6d
roca <romulo.cintra@gft.com>
2018-07-09 17:49:32 +0200

    fix : type user-agent-checker

--- packages/o-express-middleware/src/utils/user-agent-checker.ts
+++ packages/o-express-middleware/src/utils/user-agent-checker.ts
@@ -1,6 +1,6 @@
 import logger from './logger';
 
-export async function UAChecker(uaHeader: string) {
+export async function UAChecker(uaHeader: any) {
   const result = (/iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile/i).test(uaHeader);
   logger.info('info', `User Agent is Mobile : ${result}`);
   return await result;
