b18320557523d696478f0ef9c7e4f502113f0ec7
roca <romulo.cintra@gft.com>
2018-07-13 10:00:43 +0200

    feat: demo added template from the express layout

--- demos/express-js/index.js
+++ demos/express-js/index.js
@@ -4,6 +4,48 @@ var app = express();
 const route = require('@o-rango/o-express-middleware')
 let port = process.env.PORT || "3050";
 
+const tpl = `<style>
+/* Add a black background color to the top navigation */
+.topnav {
+background-color: #333;
+overflow: hidden;
+z-index : 9999999999;
+position : absolute;
+}
+/* Style the links inside the navigation bar */
+.topnav a {
+float: left;
+color: #f2f2f2;
+text-align: center;
+padding: 14px 16px;
+text-decoration: none;
+font-size: 17px;
+}
+/* Change the color of links on hover */
+.topnav a:hover {
+background-color: #ddd;
+color: black;
+}
+/* Add a color to the active/current link */
+.topnav a.active {
+background-color: #4CAF50;
+color: white;
+}
+</style>
+<header>
+<div class="topnav">
+<a class="o-link" href="/home">Home</a>
+<a class="o-link" href="/main">Main</a>
+</div>
+</header>`
+
+
+const configGlobal = {
+errorUrl : 'https://stenciljs.com/asfadada/',
+// At Moment only one template is available
+templates : { name : 'home', zone:'<!--content-->', content : `<body><html>${tpl} <!--content--> </html></body>`}
+}
+
 
 const config =  [
 	{
@@ -33,6 +75,8 @@ const config =  [
 ];
 // Set app configuration First;
 route.routerConfig(config);
+
+route.globalConfig(configGlobal);
 
 app.use(route.routerHandler);
 app.listen(port, () => {
