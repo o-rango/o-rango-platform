2f3fd3aa65798089aa1f03fb18c8a1f210e20246
ROMULO CINTRA <romulo.cintra.contractor@bbva.com>
2018-09-06 12:14:34 +0200

    feat:add axios config

--- packages/o-express-middleware/src/router/router.ts
+++ packages/o-express-middleware/src/router/router.ts
@@ -15,7 +15,7 @@ export async function routerHandler(req: Request, res: Response, next: NextFunct
 
   // Template Fetcher
   function fetchContent(url: string) {
-    return Axios.get(url);
+    return Axios.get(url , globalConfig.axiosConfig);
   }
 
   // Template Handler
@@ -24,7 +24,6 @@ export async function routerHandler(req: Request, res: Response, next: NextFunct
   if (!route && !globalConfig.errorUrl) {
     res.status(404).send('NOT FOUND');
     next();
-    return;
   }
 
   try {
